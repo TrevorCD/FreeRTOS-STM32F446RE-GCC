75cc99085be97050b16cc626f21b638cc7841cff
TrevorCD <trevorcldrwd@gmail.com>
2025-11-04 17:53:08 -0800

    added int64_t and uint64_t to stdint.h

--- include/stdint.h
+++ include/stdint.h
@@ -23,5 +23,7 @@ typedef short int16_t;
 typedef unsigned short uint16_t;
 typedef long int32_t;
 typedef unsigned long uint32_t;
-
+// Additions:
+typedef long long int64_t;
+typedef unsigned long long uint64_t;
 #endif /* FREERTOS_STDINT */
